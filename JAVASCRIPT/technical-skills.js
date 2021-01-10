27b5924beb6946df638cee115b938d2647a93c76
AliCodesJava <ali.ottawa.canada@gmail.com>
2021-01-10 17:04:48 -0500

    Technical skills page done mobile-first & modifications on about-us

---
+++ JAVASCRIPT/technical-skills.js
@@ -0,0 +1,8 @@
+const VIEWPORT_HEIGHT = window.innerHeight;
+
+const TECHNICAL_SKILLS_SECTION = document.getElementById("technical-skills");
+const HEADER = document.getElementById("header-contacts-menu");
+const FOOTER = document.getElementById("footer");
+
+const NEW_TECHNICAL_SKILLS_SECTION_HEIGHT = VIEWPORT_HEIGHT - (HEADER.offsetHeight + FOOTER.offsetHeight);
+TECHNICAL_SKILLS_SECTION.style.height = NEW_TECHNICAL_SKILLS_SECTION_HEIGHT + "px";
