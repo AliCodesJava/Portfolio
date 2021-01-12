bcad472014ca9c5daf69f14cc39fe1f41a0c37f0
AliCodesJava <ali.ottawa.canada@gmail.com>
2021-01-11 21:43:10 -0500

    another height bug fix on technicals skills page

--- JAVASCRIPT/technical-skills.js
+++ JAVASCRIPT/technical-skills.js
@@ -4,5 +4,5 @@ const TECHNICAL_SKILLS_SECTION = document.getElementById("technical-skills");
 const HEADER = document.getElementById("header-contacts-menu");
 const FOOTER = document.getElementById("footer");
 
-const NEW_TECHNICAL_SKILLS_SECTION_HEIGHT = VIEWPORT_HEIGHT - (HEADER.offsetHeight + FOOTER.offsetHeight);
+const NEW_TECHNICAL_SKILLS_SECTION_HEIGHT = VIEWPORT_HEIGHT - (HEADER.offsetHeight + FOOTER.offsetHeight + 40);
 TECHNICAL_SKILLS_SECTION.style.height = NEW_TECHNICAL_SKILLS_SECTION_HEIGHT + "px";
